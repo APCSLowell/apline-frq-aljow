fa66543ad5ec6d53936e950eb2083b06de184dc9
aljow <143014720+aljow@users.noreply.github.com>
2024-04-17 09:38:40 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,12 +1,18 @@
 public class APLine
 {
+  private int x, y, z; 
   public APLine (int a, int b, int c) {
-
+    x = a;
+    y = b;
+    z = c;
   }
   public double getSlope() {
-
+    return -x/y;
   }
-  public boolean isOnline() {
-
+  public boolean isOnline(int f, int g) {
+    if ((x*f + y*g + z) == 0) {
+      return true; 
+    }
+    return false;
   }
 }
